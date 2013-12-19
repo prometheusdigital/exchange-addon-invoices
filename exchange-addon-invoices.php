05df9687e112d7d0ac70333431420a2ec26d413d
Git User <git@petra.ithemes.com>
2013-12-19 14:59:36 -0600

    1.0.1: updated: lib/updater

--- exchange-addon-invoices.php
+++ exchange-addon-invoices.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Invoices Add-on
- * Version: 1.0.0
+ * Version: 1.0.1
  * Description: Allows you to invoice clients for services.
  * Plugin URI: http://ithemes.com/exchange/invoices/
  * Author: iThemes
