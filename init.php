13f996b664bca610265890555e8a9c7d0e23de2a
Glenn Ansley <glenn@ithemes.com>
2013-12-19 10:22:07 -0600

    Restructure invoice files

--- init.php
+++ init.php
@@ -3,6 +3,7 @@
 include_once( 'lib/product-features/class.invoices.php' );
 include_once( 'lib/functions.php' );
 include_once( 'lib/settings.php' );
+include_once( 'lib/hooks.php' );
 
 if ( ! is_admin() )
 	include_once( 'api/theme/invoice.php' );
