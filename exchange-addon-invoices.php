811d93e592a9700404ea364f2631299d8f385d31
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-03-24 17:19:31 -0400

    Don't load the invoices settings, emails are now configured in Exchange core

--- exchange-addon-invoices.php
+++ exchange-addon-invoices.php
@@ -32,7 +32,6 @@ function it_exchange_register_invoices_addon() {
 		'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/invoices50.png' ),
 		'wizard-icon'       => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/wizard-invoices.png' ),
 		'file'              => dirname( __FILE__ ) . '/init.php',
-		'settings-callback' => 'it_exchange_invoice_addon_settings_callback',
 		'category'          => 'product-type',
 		'basename'          => plugin_basename( __FILE__ ),
 		'labels'      => array(
--- init.php
+++ init.php
@@ -2,7 +2,6 @@
 
 include_once( dirname( __FILE__ ) . '/lib/product-features/class.invoices.php' );
 include_once( dirname( __FILE__ ) . '/lib/functions.php' );
-include_once( dirname( __FILE__ ) . '/lib/settings.php' );
 include_once( dirname( __FILE__ ) . '/lib/hooks.php' );
 include_once( dirname( __FILE__ ) . '/lib/tags.php' );
 include_once( dirname( __FILE__ ) . '/api/invoices.php' );
