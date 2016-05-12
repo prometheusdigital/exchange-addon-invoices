5513ad4afb7a6e47133aab543538129e7c01517d
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-05-12 11:26:33 -0400

    Tagging 1.9.1

--- exchange-addon-invoices.php
+++ exchange-addon-invoices.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Invoices Add-on
- * Version: 1.9.0
+ * Version: 1.9.1
  * Description: Allows you to invoice clients for services.
  * Plugin URI: http://ithemes.com/exchange/invoices/
  * Author: iThemes
